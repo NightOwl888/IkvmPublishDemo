fe8eb7b8d2dda934e33d6060ee119e1067f83b7f
Shad Storhaug <shad@shadstorhaug.com>
2023-02-05 01:02:19 +0700

    Added ICU4N project structure

---
+++ src/ICU4N/Foo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ICU4N
+{
+    public class Foo
+    {
+        public string GetMessage()
+        {
+            return "Hello";
+        }
+    }
+}
---
+++ tests/ICU4N.Tests.Transliterator/TestFoo.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace ICU4N.Tests.Transliterator
+{
+    public class TestFoo
+    {
+        [Test]
+        public void TestGetMessage()
+        {
+            var target = new Foo();
+
+            Assert.AreEqual("Hello", target.GetMessage());
+        }
+    }
+}
