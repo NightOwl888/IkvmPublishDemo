37845de47d13dfab48da88b8723500f23fe9fb25
Shad Storhaug <shad@shadstorhaug.com>
2023-02-05 19:28:29 +0700

    Uncommented Test11621()

--- tests/ICU4N.Tests/TestFoo.cs
+++ tests/ICU4N.Tests/TestFoo.cs
@@ -18,34 +18,34 @@ namespace ICU4N.Tests
 
         // This test throws AccessViolationException - some IKVM issue
 
-        //// Test that the decimal is shown even when there are no fractional digits
-        //[Test]
-        //public void Test11621()// throws Exception
-        //{
-        //    string pat = "0.##E0";
-
-        //    com.ibm.icu.text.DecimalFormatSymbols icuSym = new com.ibm.icu.text.DecimalFormatSymbols(java.util.Locale.US);
-        //    com.ibm.icu.text.DecimalFormat icuFmt = new com.ibm.icu.text.DecimalFormat(pat, icuSym);
-        //    icuFmt.setDecimalSeparatorAlwaysShown(true);
-        //    string icu = ((com.ibm.icu.text.NumberFormat)icuFmt).format(299792458);
-
-        //    java.text.DecimalFormatSymbols jdkSym = new java.text.DecimalFormatSymbols(java.util.Locale.US);
-        //    java.text.DecimalFormat jdkFmt = new java.text.DecimalFormat(pat, jdkSym);
-        //    jdkFmt.setDecimalSeparatorAlwaysShown(true);
-        //    string jdk = ((java.text.NumberFormat)jdkFmt).format(299792458);
-
-        //    Assert.AreEqual(jdk, icu, "ICU and JDK placement of decimal in exponent");
-        //}
-
-
+        // Test that the decimal is shown even when there are no fractional digits
         [Test]
-        public void TestCoverage()
+        public void Test11621()// throws Exception
         {
-            java.util.Currency usd = java.util.Currency.getInstance("USD");
-            Assert.AreEqual(
-                    "$",
-                    usd.getSymbol(),
-                    "USD.getSymbol()");
+            string pat = "0.##E0";
+
+            com.ibm.icu.text.DecimalFormatSymbols icuSym = new com.ibm.icu.text.DecimalFormatSymbols(java.util.Locale.US);
+            com.ibm.icu.text.DecimalFormat icuFmt = new com.ibm.icu.text.DecimalFormat(pat, icuSym);
+            icuFmt.setDecimalSeparatorAlwaysShown(true);
+            string icu = ((com.ibm.icu.text.NumberFormat)icuFmt).format(299792458);
+
+            java.text.DecimalFormatSymbols jdkSym = new java.text.DecimalFormatSymbols(java.util.Locale.US);
+            java.text.DecimalFormat jdkFmt = new java.text.DecimalFormat(pat, jdkSym);
+            jdkFmt.setDecimalSeparatorAlwaysShown(true);
+            string jdk = ((java.text.NumberFormat)jdkFmt).format(299792458);
+
+            Assert.AreEqual(jdk, icu, "ICU and JDK placement of decimal in exponent");
         }
+
+
+        //[Test]
+        //public void TestCoverage()
+        //{
+        //    java.util.Currency usd = java.util.Currency.getInstance("USD");
+        //    Assert.AreEqual(
+        //            "$",
+        //            usd.getSymbol(),
+        //            "USD.getSymbol()");
+        //}
     }
 }
